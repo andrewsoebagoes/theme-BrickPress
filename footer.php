324367cea95527dd5fe7ad16968bbabbbade06ea
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-12-04 09:10:04 +0700

    update

--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
 <?php wp_footer(); ?>
 <!--Footer upper-->
+<style>.mb-4 {margin-bottom:20px} </style>
 <section id="sp-bottom">
   <div class="container">
     <div class="row">
-      <div id="sp-bottom1" class="col-sm-6 col-md-3">
+      <div id="sp-bottom1" class="col-sm-12 col-md-3 mb-4">
         <h3 class="sp-module-title">Alamat</h3>
         <ul class="textwidget">
           <li>
@@ -15,7 +16,7 @@
           <li><i class="fa fa-fax"></i> 0123456789</li>
         </ul>
       </div>
-      <div id="sp-bottom2" class="col-sm-6 col-md-3">
+      <div id="sp-bottom2" class="col-sm-12 col-md-3 mb-4">
         <h3 class="sp-module-title">Kategori</h3>
         <ul class="textwidget">
           <?php
@@ -33,9 +34,9 @@
 
         </ul>
       </div>
-      <div id="sp-bottom3" class="col-sm-6 col-md-3">
+      <div id="sp-bottom3" class="col-sm-12 col-md-6">
         <h3 class="sp-module-title">Lokasi Kantor</h3>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.3938821118454!2d99.61107637497051!3d2.98804869698801!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30324be7f9bf7d69%3A0xcf1b538dccd3689d!2sBappeda%20Kab.%20Asahan!5e0!3m2!1sid!2sid!4v1732536555944!5m2!1sid!2sid" width="600" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.3938821118454!2d99.61107637497051!3d2.98804869698801!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30324be7f9bf7d69%3A0xcf1b538dccd3689d!2sBappeda%20Kab.%20Asahan!5e0!3m2!1sid!2sid!4v1732536555944!5m2!1sid!2sid" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
 
     </div>
